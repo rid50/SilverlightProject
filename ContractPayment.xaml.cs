0994465660c61eda263fb8323fc5431ed7fd7cff
Roman Davidenko <rid50@hotmail.com>
2012-07-19 19:30:07 +0300

    Commit it

---
+++ ContractPayment.xaml.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+using System.Windows.Data;
+using System.Collections;
+
+using System.Windows.Printing;
+using System.Windows.Browser;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.ObjectModel;
+//using System.Windows.Printing;
+
+using FaihaProject.ContractPaymentServiceReference;
+
+namespace FaihaProject
+{
+    public partial class ContractPayment : UserControl
+    {
+        private string _reportName = "r_payment_stat";
+        private string _contractId;
+        private decimal _contractValue;
+        ContractPaymentServiceClient _service;
+
+        public ContractPayment()
+        {
+            InitializeComponent();
+
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            dfContract.IsEnabledChanged += 
+                    new DependencyPropertyChangedEventHandler(dfContract_IsEnabledChanged);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e) {
+
+           // RowDefinition myRow = TopGrid.RowDefinitions[0];
+            
+           // object i = Grid.HeightProperty;
+            var serviceUrl = new Uri(Application.Current.Host.Source, "../ContractPaymentService.svc");
+            _service = new ContractPaymentServiceClient("CustomBinding_ContractPaymentService", serviceUrl.AbsoluteUri);
+//            _service = new ContractPaymentServiceClient();
+            _service.GetContractsListCompleted += new EventHandler<GetContractsListCompletedEventArgs>(service_GetContractListCompleted);
+            _service.GetContractsListAsync();
+
+            busyIndicator1.IsBusy = true;
+            dfContract.Visibility = System.Windows.Visibility.Collapsed;
+            //dgContractWorkOrder.Visibility = System.Windows.Visibility.Collapsed;
+            dgPayment.Visibility = System.Windows.Visibility.Collapsed;
+
+//            serviceUrl = new Uri(Application.Current.Host.Source, "../ContractPaymentService.svc");
+//            _service = new C("CustomBinding_WorkOrderDetailService", serviceUrl.AbsoluteUri);
+            _service.GetPaymentRunningSumCompleted += (s, eventArgs) => {
+                //ObservableCollection<PaymentRunningSumDto> list = eventArgs.Result as ObservableCollection<PaymentRunningSumDto>;
+                IList<PaymentRunningSum> paymentDetails = new List<PaymentRunningSum>();
+                decimal runningSum = 0;
+                foreach (PaymentRunningSumDto p in eventArgs.Result)
+                {
+                    PaymentRunningSum rsum = new PaymentRunningSum();
+                    runningSum += p.PaymentValue;
+
+                    rsum.PaymentNo = p.PaymentNo;
+                    rsum.PaymentDate = p.PaymentDate;
+                    rsum.PaymentValue = p.PaymentValue;
+                    rsum.RunningSum = runningSum;
+                    rsum.RemainingValue = _contractValue - runningSum;
+                    rsum.PercentPaid = runningSum / _contractValue;
+
+                    paymentDetails.Add(rsum);
+                }
+/*
+                var paymentDetails = from p in eventArgs.Result
+                                    select new PaymentRunningSum
+                                    {
+                                        PaymentNo = p.PaymentNo,
+                                        PaymentDate = p.PaymentDate,
+                                        PaymentValue = p.PaymentValue,
+                                        RunningSum = p.PaymentValue,
+                                        RemainingValue = p.PaymentValue,
+                                        PercentPaid = p.PaymentValue
+                                    };
+*/
+                dgPayment.ItemsSource = paymentDetails;
+                chartControl.DataContext = paymentDetails;
+
+
+//                dgPayment.ItemsSource = eventArgs.Result;
+                dgPayment.Visibility = System.Windows.Visibility.Visible;
+                accordionControl.Visibility = System.Windows.Visibility.Visible;
+
+                busyIndicator1.IsBusy = false;
+                //VisualStateManager.GoToState(this, "ShowState", false);
+
+/*
+                for (int i = 0; i < dgWorkOrderDetail.Columns.Count; i++)
+                {
+                    if (i == dgWorkOrderDetail.Columns.Count - 2 || i == dgWorkOrderDetail.Columns.Count - 1)
+                    {
+                        dgWorkOrderDetail.Columns[i].Width = new DataGridLength(dgWorkOrderDetail.Columns[i].ActualWidth * 2);
+                    }
+                }
+*/
+            };
+        }
+
+        void service_GetContractListCompleted(object sender, GetContractsListCompletedEventArgs e)
+        {
+//            busyIndicator1.IsBusy = false;
+            dfContract.Visibility = System.Windows.Visibility.Visible;
+
+            try
+            {
+                if (e.Error == null)
+                {
+                    dfContract.ItemsSource = e.Result;
+
+//                    dfContractWorkOrder.Header = (dfContractWorkOrder.CurrentIndex + 1).ToString() + "/" +
+//                                                (dfContractWorkOrder.ItemsSource as Collection<ContractWorkOrderDto>).Count();
+                    //dfContractWorkOrder.Header = e.Result;
+//                    dfContractWorkOrder.Header = (e.Result[0] as ContractWorkOrderDto).ClassID;
+                    //ContractWorkOrderDto dto = dfContractWorkOrder.CurrentItem as ContractWorkOrderDto;
+                    //_contractId = dto.ContractID;
+
+                    //var requstXPS = new RequestXPS();
+                    //"[Contract No] = '2003 / 2002 (135)'"
+                    //requstXPS.getXPSReport(_reportName, "[Contract No] = '" + _contractId + "'", xps_viewer, statusText);
+                }
+                else
+                {
+                    if (e.Error is FaultException)
+                    {
+                        //var fault = e.Error as FaultException<ExceptionDetail>;
+                        var fault = e.Error as FaultException;
+                        //MessageBox.Show(fault.Detail.Message);
+                        MessageBox.Show(" fault.Message: " + fault.Message.ToString() + Environment.NewLine +
+                                        " fault.Reason: " + fault.Reason.ToString() + Environment.NewLine +
+                                        " fault.Code: " + fault.Code.ToString());
+                        //ErrorPanel.DataContext = fault;
+                    }
+                    else
+                    {
+                        Exception ex = e.Error.InnerException;
+                        string msg = e.Error.Message;
+                        while (ex != null)
+                        {
+                            msg += Environment.NewLine + ex.ToString();
+                            ex = ex.InnerException;
+                        }
+                        MessageBox.Show(" Error Message: " + msg);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+//        private void dgContractWorkOrder_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void dfContract_CurrentItemChanged(object sender, EventArgs e)
+        {
+            dfContract.Header = (dfContract.CurrentIndex + 1).ToString() + "/" +
+                                        (dfContract.ItemsSource as Collection<ContractDto>).Count();
+
+            busyIndicator1.IsBusy = true;
+
+            ContractDto dto = ((sender as DataForm).CurrentItem) as ContractDto;
+            _contractId = dto.ContractID;
+            _contractValue = dto.ContractValue;
+
+            var requstXPS = new RequestXPS();
+            //"[Contract No] = '2003 / 2002 (135)'"
+            requstXPS.getXPSReport(_reportName, "[Contract No] = '" + _contractId + "'",
+                                                reportPanel, xps_viewer, statusText, false);
+
+            _service.GetPaymentRunningSumAsync(dto.ContractID);
+        }
+
+        private void dfContract_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!IsEnabled)
+            {
+                VisualStateManager.GoToState(dfContract, "Disabled", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(dfContract, "Normal", true);
+            }
+        }
+
+        private void Preview_Click(object sender, RoutedEventArgs e)
+        {
+            HtmlPopupWindowOptions options = new HtmlPopupWindowOptions();
+            options.Left = 0;
+            options.Top = 0;
+            options.Width = 1024;
+            options.Height = 800;
+            options.Resizeable = true;
+            options.Scrollbars = true;
+            options.Menubar = false;
+            options.Toolbar = true;
+
+            Uri url = new Uri(Application.Current.Host.Source, "../XPS/" +
+                        hashPredicateExpression("[Contract No] = '" + _contractId + "'") + "_" + _reportName + ".xps");
+
+            HtmlPage.PopupWindow(url, "_blank", options);
+        }
+
+        private string hashPredicateExpression(string predicate) {
+            byte[] hash;
+
+            //byte[] data = Encoding.UTF8.GetBytes(predicate);
+            UTF8Encoding enc = new UTF8Encoding();
+            byte[] data = enc.GetBytes(predicate);
+            SHA1 sha = new SHA1Managed();
+            hash = sha.ComputeHash(data);
+            //System.BitConverter.ToString(hash).Replace("-", "").ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        private void dfContract_ContentLoaded(object sender, DataFormContentLoadEventArgs e)
+        {
+            new Utils().setToolTip(dfContract);
+            accordionControl.SelectedItem = accordionChartItem;
+        }
+
+        public class PaymentRunningSum
+        {
+            public short PaymentNo { get; set; }
+
+            public DateTime PaymentDate { get; set; }
+
+            public decimal PaymentValue { get; set; }
+
+            public decimal RunningSum { get; set; }
+
+            public decimal RemainingValue { get; set; }
+
+            public decimal PercentPaid { get; set; }
+        }
+    }
+}
---
+++ PaymentRepository.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+using NHibernate.Type;
+using NHibernate.SqlCommand;
+using NHibernate.Linq;
+
+using FaihaProject.Dal.Entities;
+using FaihaProject.Dal.Dto;
+
+#pragma warning disable 219
+
+
+namespace FaihaProject.Dal.Repositories
+{
+    public class PaymentRepository
+    {
+        public ICollection<PaymentPenaltyDto> DLookUpPaymentPenalty(string contractID, short paymentNo)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                string sql = @" SELECT distinct 
+                        new TotalPayment(wo.ContractID, wo.WOID, wo.WOID,
+                                        sum(wod.RealQuantity * wod.UnitPrice), 0m)
+                        FROM WorkOrder wo, WorkOrderDetail wod, PaymentDetail p
+	                    WHERE (wo.HandOverWODate is not null) AND
+                             (wo.ContractID = wod.ContractID AND wo.WOID = wod.WOID) AND
+                             (wo.ContractID = p.ContractID AND wo.WOID = p.WOID) AND
+                             (p.ContractID = '" + contractID + "' And p.PaymentNo = " + paymentNo + ")" +
+                        " GROUP BY wo.ContractID, wo.WOID";
+
+
+                var totalPayment = session.CreateQuery(sql)
+                    //.SetMaxResults(10)
+                    .List<TotalPayment>();
+
+                //return totalPayment;
+
+                sql = @" SELECT distinct new PaymentDto(wo.ContractID, p.PaymentNo, wo.WOID, 0m, wo.Discount,
+                                wo.HandOverWODate, wo.WODate, wo.WOAge, wo.Contract.ContractValue, wo.WODate)
+                        FROM WorkOrder wo, PaymentDetail p
+                        WHERE (wo.ContractID = p.ContractID AND wo.WOID = p.WOID) AND
+                            (p.ContractID = '" + contractID + "' And p.PaymentNo = " + paymentNo + ")";
+
+                ICollection<PaymentDto> paymentDto = session.CreateQuery(sql)
+                    //.SetMaxResults(10)
+                    .List<PaymentDto>();
+
+                //Console.WriteLine("Total records: {0}", paymentDto.Count());
+
+                foreach (TotalPayment t in totalPayment)
+                {
+                    foreach (PaymentDto p in paymentDto)
+                    {
+                        if (t.ContractID == p.ContractID && t.WOID == p.WOID)
+                        {
+                            t.PaymentNo = p.PaymentNo;
+                            t.Discount = p.Discount;
+                            //t.Delay = p.Delay;
+                            t.WOAge = p.WOAge;
+                            t.HandOverWODate = p.HandOverWODate;
+                            t.WODate = p.WODate;
+                            t.ContractValue = p.ContractValue;
+                            //Console.WriteLine("Delay: {0}, Age: {1}, Value: {2}", p.Delay, p.WOAge, p.ContractValue);
+                            break;
+                        }
+                    }
+                }
+
+                //return totalPayment;
+
+                var total =
+                        from td in totalPayment
+                        where td.ContractID == contractID && td.PaymentNo == paymentNo
+                        group td by new { td.ContractID, td.PaymentNo } into grouping
+                        select new
+                        {
+                            grouping.Key,
+                            total = grouping.Sum(t => t.Total)
+                        };
+
+                var penalty =
+                        from td in totalPayment
+                        where td.ContractID == contractID && td.PaymentNo == paymentNo
+                        group td by new { td.ContractID, td.PaymentNo } into grouping
+                        select new
+                        {
+                            grouping.Key,
+                            penalty = grouping.Sum(t => getPenalty(t.ContractValue, t.WOAge, t.Delay, t.Total))
+                        };
+
+                ICollection<PaymentPenaltyDto> dto = new List<PaymentPenaltyDto>();
+                PaymentPenaltyDto pp = new PaymentPenaltyDto();
+                foreach (var grp in total)
+                {
+                    pp.TotalValue = decimal.Round(grp.total, 3);
+                    break;
+                }
+
+                foreach (var grp in penalty)
+                {
+                    pp.Penalty = decimal.Round(grp.penalty, 3);
+                    break;
+                }
+
+                dto.Add(pp);
+
+                return dto;
+            }
+        }
+
+
+        private decimal getPenalty(decimal? contractValue, int? contractAge, int? delay, decimal? total)
+        {
+            if (!(contractValue == null || contractAge == null || delay == null || total == null)) {
+                if (delay > 0) {
+                    if (total * 0.25m * delay / contractAge >= contractValue * 0.1m)
+                        return (decimal)contractValue * 0.1m;
+                    else
+                        return (decimal)(total * 0.25m * delay / contractAge);
+                } else
+                    return 0;
+            } else
+                return 0;
+        }
+
+        public ICollection<PaymentDto> GetPayment()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var ic = session.CreateCriteria(typeof(Contractor))
+                    //.SetMaxResults(10)
+                    .CreateCriteria("Contracts", "c")
+                    .CreateAlias("Payments", "p")
+                    .SetProjection(Projections.Distinct(Projections.ProjectionList()
+                        .Add(Projections.Property("c.ContractID"), "ContractID")
+                        //.Add(Projections.Property("p.PaymentNo"), "PaymentNo")
+                        .Add(Projections.Property("c.ClassID"), "ClassID")
+                        .Add(Projections.Property<Contractor>(c => c.ContractorName).As("ContractorName"))
+                        .Add(Projections.Property("c.ContractTitle"), "ContractTitle")
+                        .Add(Projections.Property("c.TenderTitle"), "TenderTitle")
+                        .Add(Projections.Property("p.PaymentDate"), "PaymentDate")
+                        .Add(Projections.Property("p.Description"), "Description")
+                        .Add(Projections.Property("c.ContractValue"), "ContractValue")
+                        .Add(Projections.Property("c.ContractPeriod"), "ContractPeriod")
+                        .Add(Projections.Property("c.ContractStartDate"), "ContractStartDate")
+                        .Add(Projections.Property("c.ContractEndDate"), "ContractEndDate")
+                        .Add(Projections.Property("c.ExtensionEndDate"), "ExtensionEndDate")
+
+                    ))
+                    .SetResultTransformer(new AliasToBeanResultTransformer(typeof(PaymentDto)))
+                    .List<PaymentDto>();
+                return ic;
+            }
+        }
+
+        public ICollection<PaymentDetailDto> GetPaymentDetail(string contractID)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+//                var ic = session.CreateCriteria(typeof(Contractor))
+                var ic = session.CreateCriteria(typeof(Payment), "p")
+                    //.SetMaxResults(10)
+                    //.CreateCriteria("Contracts", "c")
+                    //.CreateCriteria("Payments", "p")
+                    .Add(Restrictions.Eq("p.ContractID", contractID))
+                    .SetProjection(Projections.Distinct(Projections.ProjectionList()
+                        .Add(Projections.Property("p.ContractID"), "ContractID")
+                        .Add(Projections.Property("p.PaymentNo"), "PaymentNo")
+                        .Add(Projections.Property("p.D6"), "D6")
+                        .Add(Projections.Property("p.D7"), "D7")
+                        .Add(Projections.Property("p.D8"), "D8")
+                        .Add(Projections.Property("p.C9"), "C9")
+                        .Add(Projections.Property("p.I10"), "I10")
+                    //                        .Add(Projections.SqlProjection(
+                    //                                "=DLookUp(\"[Total]\",\"q_payment_total_for_accounting\"," +
+                    //                                        "\"[Contract No] = '\" & p2_.[Contract No] & \"' And [Payment No] = p2_.[Payment No]\")",
+                    //                                new string[] { "C11" }, new IType[] { NHibernateUtil.Currency }), "C11")
+
+                        .Add(Projections.Property("p.C12"), "C12")
+                        .Add(Projections.Property("p.C13"), "C13")
+                        .Add(Projections.Property("p.C14"), "C14")
+                        .Add(Projections.Property("p.C15"), "C15")
+                        .Add(Projections.Property("p.C16"), "C16")
+                        .Add(Projections.Property("p.C18"), "C18")
+                        .Add(Projections.Property("p.C20"), "C20")
+                        .Add(Projections.Property("p.C21"), "C21")
+                        .Add(Projections.Property("p.C22"), "C22")
+                        .Add(Projections.Property("p.C23"), "C23")
+                        .Add(Projections.Property("p.C24"), "C24")
+                    ))
+                    .SetResultTransformer(new AliasToBeanResultTransformer(typeof(PaymentDetailDto)))
+                    .List<PaymentDetailDto>();
+                return ic;
+            }
+        }
+
+    }
+}
